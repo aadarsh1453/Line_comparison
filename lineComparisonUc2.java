22b38386ec3a01c5265f28d4725a09335e9c0512
Aadarshkumar Sharma <113667076+aadarsh1453@users.noreply.github.com>
2024-05-08 10:57:16 +0530

    UC2

---
+++ lineComparisonUc2.java
@@ -0,0 +1,25 @@
+  public class Line_Comparison_UC2 {
+    
+    public static void main(String[] args) {
+        
+        int x1l1 = 2;int x2l1 = 6;int y1l1 = 4;int y2l1 = 9;
+        int x1l2 = 2;int x2l2 = 6;int y1l2 = 4;int y2l2 = 8;
+
+        double length_line1 = Math.sqrt(Math.pow(x2l1-x1l1, 2) + Math.pow(y2l1-y1l1, 2));
+        System.out.println("Length of line is :"+length_line1);
+
+        double length_line2 = Math.sqrt(Math.pow(x2l2-x1l2, 2) + Math.pow(y2l2-y1l2, 2));
+        System.out.println("Length of line is :"+length_line2);
+
+        if(length_line1 == length_line2){
+            System.out.println("Length of  both lines are equal");
+        }
+        else{
+            System.out.println("Length of  both lines are not equal");
+
+        }
+        
+
+        
+    }
+}
